f293bf2283b661264c5e3b720ed2b5e7d7e78052
hamdouni <brahim@hamdouni.com>
2017-10-27 16:03:25 +0200

    dwm tag more explicit + webapps installation with chromium and tag aware

--- programs/dwm-6.0/config.h
+++ programs/dwm-6.0/config.h
@@ -17,16 +17,19 @@ static const Bool showbar           = True;     /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5" };
+static const char *tags[] = { "1_main", "2_code", "3_backlog", "4_master", "5_agenda", "6_gmail", "7_music" };
 
 static const Rule rules[] = {
-	/* class      instance    title       tags mask     isfloating   monitor */
-/*	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
-*/
-    { "Wicd-client.py", NULL, NULL,       0,            True,        -1 },
-    { "Update-manager", NULL, NULL,       0,            True,        -1 },
-	{ NULL,             NULL, "Hangouts", 0,            True,        -1 },
+	/* class      		instance    title       	tags mask     isfloating	monitor */
+    { "Wicd-client.py", NULL, 		NULL,       		0,          True,	  	-1 },
+    { "Update-manager", NULL, 		NULL,       		0,          True,	  	-1 },
+	{ NULL,             NULL, 		"Hangouts", 		0,          True,	  	-1 },
+	{ "music",			NULL, 		NULL,			1 << 6, 		False, 		-1 },
+    { "gmail", 			NULL, 		NULL,			1 << 5, 		False, 		-1 },
+    { "agenda",			NULL, 		NULL,			1 << 4, 		False, 		-1 },
+    { "master", 		NULL, 		NULL,	 		1 << 3, 		False, 		-1 },
+    { "backlog",		NULL,		NULL,			1 << 2, 		False, 		-1 },
+    { "Code", 			"code", 	NULL, 			1 << 1, 		False, 		-1 },
 };
 
 /* layout(s) */
