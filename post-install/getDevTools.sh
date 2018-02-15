b73cff61509749ee5e269ed10539f51380e4de3b
hamdouni <brahim@hamdouni.com>
2018-02-15 15:02:47 +0100

    post-install docker-compose

--- post-install/getDevTools.sh
+++ post-install/getDevTools.sh
@@ -22,4 +22,7 @@ sudo apt update
 sudo apt install -y docker-ce code
 sudo usermod -aG docker $USER
 
+# install docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
