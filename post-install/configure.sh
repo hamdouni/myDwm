2977f0c1a46b361dfe0227bfbfb4babf6978bf3b
Brahim Hamdouni <brahim@hamdouni.com>
2018-11-02 23:30:23 +0100

    Move post configuration to the right place

--- post-install/configure.sh
+++ post-install/configure.sh
@@ -7,3 +7,17 @@ git config --global push.default simple
 
 # make docker usable withoud sudo
 sudo usermod -aG docker $USER
+
+# make terminology my default
+sudo update-alternatives --quiet --set x-terminal-emulator /usr/bin/terminology
+
+# make pcmanfm default file manager
+sudo update-alternatives --quiet --remove-all x-file-manager 2>/dev/null
+sudo update-alternatives --quiet --install /usr/bin/x-file-manager x-file-manager /usr/bin/pcmanfm 1000
+sudo update-alternatives --quiet --set x-file-manager /usr/bin/pcmanfm
+xdg-mime default Pcmanfm.desktop inode/directory
+
+# make firefox my default
+sudo update-alternatives --quiet --set x-www-browser /usr/bin/firefox
+sudo update-alternatives --quiet --set gnome-www-browser /usr/bin/firefox
+xdg-settings set default-web-browser firefox.desktop
