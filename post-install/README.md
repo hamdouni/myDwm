0d8404ec4d6893f7130f72a5a025e3de55164986
Brahim Hamdouni <brahim@hamdouni.com>
2018-11-02 23:10:35 +0100

    Simplify the way to install dev tools

--- post-install/README.md
+++
@@ -1,9 +0,0 @@
-# DEV CONFIGURATION
-
-For Visual Studio Code and Docker Community Edition :
-
-- configures repositories
-- installs packages
-
-For Git, configure name and email.
-
--- post-install/configure.sh
+++ post-install/configure.sh
@@ -1,4 +1,9 @@
+#!/bin/sh
+
+# configure git
 git config --global user.name "Brahim Hamdouni"
 git config --global user.email "brahim@hamdouni.com"
 git config --global push.default simple
 
+# make docker usable withoud sudo
+sudo usermod -aG docker $USER
