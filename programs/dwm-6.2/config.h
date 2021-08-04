cbcd226c5af500968e0673c8fcb0f3d085ecb330
Brahim Hamdouni <brahim@hamdouni.com>
2021-08-04 10:27:52 +0200

    Make Gimp not floating

--- programs/dwm-6.2/config.h
+++ programs/dwm-6.2/config.h
@@ -35,9 +35,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      		instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     		NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  		NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Update-manager", NULL, 		NULL,		0,            True,	  	   -1 },
+	{ "Update-manager", NULL, 		NULL,		0,            1,	  	   -1 },
 	{ "KeePassXC",		NULL,		NULL,		0,			  1,           -1},
 };
 
