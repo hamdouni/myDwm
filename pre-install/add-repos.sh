78cb1249e3fe8a3a4658e11fca78def356a69ed1
Brahim Hamdouni <brahim@hamdouni.com>
2022-04-20 15:55:35 +0200

    add golang ubuntu repo

--- pre-install/add-repos.sh
+++ pre-install/add-repos.sh
@@ -5,5 +5,12 @@ res=$(dpkg-query --show --showformat='${db:Status-Status}\n' "docker-ce")
 if [ "$res" != "installed" ]
 then
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+fi
+
+# golang repo
+res=$(dpkg-query --show --showformat='${db:Status-Status}\n' "golang-go")
+if [ "$res" != "installed" ]
+then
+    sudo add-apt-repository -y ppa:longsleep/golang-backports
 fi
