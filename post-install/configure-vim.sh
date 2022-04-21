06a8174aa9d969856aec07b758e0119daf24c685
Brahim Hamdouni <brahim@hamdouni.com>
2022-04-21 10:59:14 +0200

    vim: fix install plugins

---
+++ post-install/configure-vim.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# install vundle as vim plugin manager then install plugins defined in .vimrc
+# vim-go : [Fatih's awesome vim plugin to code in go](https://github.com/fatih/vim-go)
+# vim-air : [Bailey's wonderfull light powerline replacement](https://github.com/vim-airline/vim-airline)
+
+if [ ! ~/.vim/bundle/Vundle.vim ]; then
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+fi
+
+# set as default editor
+sudo update-alternatives --set editor /usr/bin/vim.basic
+
