c9ce1f84863b4d7282e4712a1b8cfb2a8da17da4
Brahim Hamdouni <brahim@hamdouni.com>
2022-04-23 20:37:35 +0200

    vim: make it more readable

--- programs/st/config.h
+++ programs/st/config.h
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.9;
+float alpha = 0.97;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
